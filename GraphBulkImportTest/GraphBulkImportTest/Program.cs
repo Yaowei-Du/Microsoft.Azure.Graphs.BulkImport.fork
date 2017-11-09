5d3377a60b82cd01a82cba3b22279facffbc68d2
LuisBosquez <luisdbosquez@gmail.com>
2017-11-09 12:04:22 -0800

    Initial push

---
+++ GraphBulkImportTest/GraphBulkImportTest/Program.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using Microsoft.Azure.Graphs;
+using Microsoft.Azure.Graphs.BulkImport;
+using Microsoft.Azure.Graphs.Elements;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace GraphBulkImportTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.TestBulkInSertAsync().Wait();
+            
+        }
+
+
+        public static async Task TestBulkInSertAsync()
+        {
+            DocumentClient client = new DocumentClient(
+                new Uri(ConfigurationManager.AppSettings["DocDBEndPoint"]),
+                ConfigurationManager.AppSettings["DocDBKey"],
+                new ConnectionPolicy { ConnectionMode = ConnectionMode.Direct, ConnectionProtocol = Protocol.Tcp });
+
+            DocumentCollection collection = await client.ReadDocumentCollectionAsync(
+                    UriFactory.CreateDocumentCollectionUri(
+                        ConfigurationManager.AppSettings["DocDBDatabase"],
+                        ConfigurationManager.AppSettings["DocDBCollection"]))
+                .ConfigureAwait(false);
+
+            GraphBulkImport graphBulkImporter = new GraphBulkImport(client, collection, true);
+
+            await graphBulkImporter.InitializeAsync();
+
+            int count = 100;
+
+            GraphBulkImportResponse vResponse =
+                await graphBulkImporter.BulkImportVerticesAsync(GenerateVertices(count), false).ConfigureAwait(false);
+            GraphBulkImportResponse eResponse =
+                await graphBulkImporter.BulkImportEdgesAsync(GenerateEdges(count), false).ConfigureAwait(false);
+
+            Console.WriteLine("\nSummary for batch");
+            Console.WriteLine("--------------------------------------------------------------------- ");
+            Console.WriteLine(
+                "Inserted {0} vertices @ {1} edges @ {2} writes/s, {3} RU/s in {4} sec)",
+                vResponse.NumberOfVerticesImported,
+                eResponse.NumberOfEdgesImported,
+                Math.Round(
+                    (vResponse.NumberOfVerticesImported + eResponse.NumberOfEdgesImported) /
+                    (vResponse.TotalTimeTaken.TotalSeconds + eResponse.TotalTimeTaken.TotalSeconds)),
+                Math.Round(
+                    (vResponse.TotalRequestUnitsConsumed + eResponse.TotalRequestUnitsConsumed) /
+                    (vResponse.TotalTimeTaken.TotalSeconds + eResponse.TotalTimeTaken.TotalSeconds)),
+                vResponse.TotalTimeTaken.TotalSeconds + eResponse.TotalTimeTaken.TotalSeconds);
+            Console.WriteLine(
+                "Average RU consumption per document: {0}",
+                (vResponse.TotalRequestUnitsConsumed + eResponse.TotalRequestUnitsConsumed) /
+                (vResponse.NumberOfVerticesImported + eResponse.NumberOfVerticesImported));
+            Console.WriteLine("---------------------------------------------------------------------\n ");
+
+            Console.ReadLine();
+        }
+
+        private static IEnumerable<Edge> GenerateEdges(int count)
+        {
+            for (int i = 0; i < count - 1; i++)
+            {
+                Edge e = new Edge(
+                    "e" + i,
+                    "knows",
+                    i.ToString(),
+                    (i + 1).ToString(),
+                    "vertex",
+                    "vertex",
+                    i,
+                    i + 1);
+
+                e.AddProperty("duration", i);
+
+                yield return e;
+            }
+        }
+
+        private static IEnumerable<Vertex> GenerateVertices(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Vertex v = new Vertex(i.ToString(), "vertex");
+                v.AddProperty(new VertexProperty("pk", i));
+                v.AddProperty(new VertexProperty("name", "name" + i));
+
+                yield return v;
+            }
+        }
+    }
+}
--- README.GraphBulkImport.txt
+++ README.GraphBulkImport.txt
@@ -1,15 +1,14 @@
-# Microsoft.Azure.Graphs.BulkImport Sample
 Notes for using the Microsoft.Azure.Graphs.BulkImport library:
 
 0. Please find the .nupkg for the library along with this read me. Make sure to reference all .dlls within this package, in your code.
 
-1. This package comes with it's own internal version of the Microsoft.Azure.Documents.Client. Please donâ€™t override that with one available on public Document SDK nuget.
+1. This package comes with it's own internal version of the Microsoft.Azure.Documents.Client. Please don’t override that with one available on public Document SDK nuget.
 We will get rid of this dependency after we release the next SDK.
 
 2. If you are ingesting a lot of documents, higher RUs and a high memory VM is recommended. 
 
 3. The API calls accept either an IEnumerable of Vertex or IEnumerable of Edges, so if you want do bulk import in a streaming fashion, the recommendation is to create batches of Vertices and Edges, and call these APIs periodically.
-Note that, while adding edges, we donâ€™t check for the existence of the source or destination vertices. So one can create edges before the corresponding vertices, but the onus is on the user to make sure that they import the source
+Note that, while adding edges, we don’t check for the existence of the source or destination vertices. So one can create edges before the corresponding vertices, but the onus is on the user to make sure that they import the source
  and destination vertex eventually. 
 
 4.  Settng useFlatProperty = true will create vertex properties as simple properties. Setting it false will create vertex properties as an array. Use the second model, only if you need support for multi-valued properties, i.e., one 
@@ -31,18 +30,3 @@ property can have multiple values.
         }
 
 5. Take a look at the GraphBulkImportTest sample solution for an example usage.
-
-
-# Contributing
-
-This project welcomes contributions and suggestions.  Most contributions require you to agree to a
-Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
-the rights to use your contribution. For details, visit https://cla.microsoft.com.
-
-When you submit a pull request, a CLA-bot will automatically determine whether you need to provide
-a CLA and decorate the PR appropriately (e.g., label, comment). Simply follow the instructions
-provided by the bot. You will only need to do this once across all repos using our CLA.
-
-This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
-For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
-contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
